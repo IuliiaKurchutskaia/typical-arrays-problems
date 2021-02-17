25025f08300e8859db3c4e1daeb25c5fe37ab98f
IuliiaKurchitskaia <53300963+IuliiaKurchutskaia@users.noreply.github.com>
2021-02-17 19:42:08 +0300

    feat: add implement of max,min,avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+
+let minNumber = (a, b) => {
+  return a < b ? a : b
+};
+if (typeof array != "undefined" && array != null && array.length != null && array.length > 0){
+return array.reduce(minNumber);
+}
+return 0;
 }
 
 exports.max = function max (array) {
+  let maxNumber = (a, b) => {
+    return a > b ? a : b
+  };
+  if (typeof array != "undefined" && array != null && array.length != null && array.length > 0){
+  return array.reduce(maxNumber);
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  
+
+  if (typeof array != "undefined" && array != null && array.length != null && array.length > 0){
+ 
+      let sum = 0;
+      for (let number of array) {
+      sum += number;
+      }
+    return sum/(array.length);
+  
+  }
+    return 0;
+  }
